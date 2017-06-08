31b107349b15e8c876c2eff145156f8c98b5272f
Andrew Perry <adp2741@gmail.com>
2017-06-08 12:50:31 -0400

    Working on MVC 6

--- src/TheWorld/wwwroot/script/site.js
+++ src/TheWorld/wwwroot/script/site.js
@@ -1,3 +1,32 @@
-﻿var ele = document.getElementById("username");
-ele.innerHTML = "Andrew D. Perry";
+﻿(function() {
+    "use strict";
 
+    //var ele = $("#username");
+    //ele.text("Andrew D. Perry");
+
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.css("background-color", "#888");
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.css("background-color", "");
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    console.log(me.text());
+    //})
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+})();
